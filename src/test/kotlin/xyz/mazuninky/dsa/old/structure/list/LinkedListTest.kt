f5b4bda537788e9c6f63d40b4944de5e8bb5f068
Konstantin Mazunin <konstantin.mazunin@t-systems.com>
2019-11-08 18:15:55 +0300

    Comment linked list test

--- src/test/kotlin/xyz/mazuninky/dsa/old/structure/list/LinkedListTest.kt
+++ src/test/kotlin/xyz/mazuninky/dsa/old/structure/list/LinkedListTest.kt
@@ -1,25 +1,22 @@
 package xyz.mazuninky.dsa.old.structure.list
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-import kotlin.test.assertFailsWith
-
-class LinkedListTest {
-    @Test
-    fun addTest() {
-        SingleLinkedList<Int>().apply {
-            val item = 1
-            add(item)
-            assertEquals(item, get(0))
-        }
-    }
-
-    @Test
-    fun getTest() {
-        assertFailsWith(IndexOutOfBoundsException::class) {
-            SingleLinkedList<Int>().apply {
-                get(2)
-            }
-        }
-    }
-}
+//
+//class LinkedListTest {
+//    @Test
+//    fun addTest() {
+//        SingleLinkedList<Int>().apply {
+//            val item = 1
+//            add(item)
+//            assertEquals(item, get(0))
+//        }
+//    }
+//
+//    @Test
+//    fun getTest() {
+//        assertFailsWith(IndexOutOfBoundsException::class) {
+//            SingleLinkedList<Int>().apply {
+//                get(2)
+//            }
+//        }
+//    }
+//}
