e06e49a12887a0b05aeb135e99fa7b5291e33c62
Konstantin Mazunin <konstantin.mazunin@t-systems.com>
2019-11-08 14:32:33 +0300

    Implement jump search

--- src/main/kotlin/xyz/mazuninky/dsa/algorithm/JumpSearch.kt
+++ src/main/kotlin/xyz/mazuninky/dsa/algorithm/JumpSearch.kt
@@ -6,21 +6,36 @@ import kotlin.math.roundToInt
 import kotlin.math.sqrt
 import kotlin.math.truncate
 
-fun <T> KList<T>.jumpSearch(item: T, comparator: Comparator<T>): Boolean{
+fun <T> KList<T>.jumpSearch(item: T, comparator: Comparator<T>): Boolean {
     val blockSize = truncate(sqrt(size.toDouble())).roundToInt()
 
     val blocks = floor((size.toDouble() / blockSize)).roundToInt()
 
-    for (blockNum in 0..blocks) {
-        val compare = comparator.compare(item, get(blockNum * blockSize))
-//        if(compare > 0)
-
-    }
-
-    return false
+    return nextBlock(item, comparator, 0, blockSize, blocks)
 }
 
 
-fun <T> KList<T>.nextBlock(item: T, comparator: Comparator<T>) {
+fun <T> KList<T>.nextBlock(item: T, comparator: Comparator<T>, blockNum: Int, blockSize: Int, blocks: Int): Boolean {
+    if (blockNum == blockSize)
+        return false
 
+    val compare = comparator.compare(item, get(blockNum * blockSize))
+
+    if (compare == 0)
+        return true
+
+    if (compare > 0) {
+        return nextBlock(item, comparator, blockNum + 1, blockSize, blocks)
+    } else {
+        if (blockNum == 0)
+            return false
+
+        for (i in (blockNum * blockSize - 1) downTo (blockNum * blockSize + 1)) {
+            if (comparator.compare(item, get(i)) == 0) {
+                return true
+            }
+        }
+    }
+
+    return false
 }
